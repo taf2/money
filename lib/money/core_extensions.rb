c4bf5e4be6904ec16cd7940fa07bbe3ab72b3408
Brandon Keepers <brandon@collectiveidea.com>
2008-10-15 10:26:02 -0400

    Update the #to_money methods to take a precision arg, and guess the precision for floats and strings

--- lib/money/core_extensions.rb
+++ lib/money/core_extensions.rb
@@ -1,26 +1,51 @@
-# Allows Writing of 100.to_money for +Numeric+ types
-#   100.to_money => #<Money @cents=10000>
-#   100.37.to_money => #<Money @cents=10037>
 class Numeric
-  def to_money
-    Money.new(self * 100)
+  # Convert the number to a +Money+ object.
+  #
+  #   100.to_money      #=> #<Money @cents=10000>
+  #
+  # Takes an optional precision, which defaults to 2
+  def to_money(precision = 2)
+    Money.new(self * 10**precision, Money.default_currency, precision)
   end
 end
 
-# Allows Writing of '100'.to_money for +String+ types
-# Excess characters will be discarded
-#   '100'.to_money => #<Money @cents=10000>
-#   '100.37'.to_money => #<Money @cents=10037>
+class Float
+  # Convert the float to a +Money+ object.
+  #
+  #   3.75.to_money     #=> #<Money @cents=375>
+  #
+  # It takes an optional precision, which defaults to 2 or the number of digits
+  # after the decimal point if it's more than 2.
+  def to_money(precision = nil)
+    to_s.to_money(precision)
+  end
+end
+
 class String
-  def to_money
+  # Convert the String to a +Money+ object.
+  #
+  #   '100'.to_money    #=> #<Money @cents=10000>
+  #   '100.37'.to_money #=> #<Money @cents=10037>
+  #
+  # It takes an optional precision argument which defaults to 2 or the number of
+  # digits after the decimal point if it's more than 2.
+  #
+  #   '3.479'.to_money  # => #<Money @cents=3479 @precision=3>
+  #
+  def to_money(precision = nil)
     # Get the currency
     matches = scan /([A-Z]{2,3})/ 
     currency = matches[0] ? matches[0][0] : Money.default_currency
     
+    if !precision
+      precision = scan(/\.(\d+)/).to_s.length
+      precision = 2 if precision < 2
+    end
+    
     # Get the cents amount
     matches = scan /(\-?\d+(\.(\d+))?)/
-    cents = matches[0] ? (matches[0][0].to_f * 100) : 0
+    cents = matches[0] ? (matches[0][0].to_f * 10**precision) : 0
     
-    Money.new(cents, currency)
+    Money.new(cents, currency, precision)
   end
 end
--- tests/core_extensions_test.rb
+++ tests/core_extensions_test.rb
@@ -8,10 +8,14 @@ class CoreExtensionsTest < Test::Unit::TestCase
   def setup
   end
 
-  def test_numeric_conversion    
+  def test_numeric_conversion
     assert_equal Money.new(10000), 100.to_money
     assert_equal Money.new(10038), 100.38.to_money
     assert_equal Money.new(-10000), -100.to_money
+  end
+
+  def test_numeric_conversion_with_precision
+    assert_equal Money.new(1000, Money.default_currency, 3), 1.to_money(3)
   end
     
   def test_string_conversion
@@ -21,6 +25,21 @@ class CoreExtensionsTest < Test::Unit::TestCase
     assert_equal Money.new(100, 'CAD'), "CAD $1.00".to_money
     assert_equal Money.new(-10000), "-100".to_money
     assert_equal Money.new(410), "4.10".to_money
+  end
+  
+  def test_string_with_other_precision
+    assert_equal Money.new(500, 'USD', 2), '5'.to_money
+    assert_equal Money.new(500, 'USD', 2), '5.0'.to_money
+    assert_equal Money.new(500, 'USD', 2), '5.00'.to_money
+    assert_equal Money.new(5055, 'USD', 3), '5.055'.to_money
+    assert_equal Money.new(506, 'USD', 2), '5.055'.to_money(2)
+  end
+  
+  def test_float_conversion_with_precision
+    assert_equal Money.new(500, 'USD', 2), 5.0.to_money
+    assert_equal Money.new(500, 'USD', 2), 5.00.to_money
+    assert_equal Money.new(5055, 'USD', 3), 5.055.to_money
+    assert_equal Money.new(506, 'USD', 2), 5.055.to_money(2)
   end
   
   def test_nil
