38e469d9b1f8aba955d4ef7a4938480627dfcaf7
Brandon Keepers <brandon@collectiveidea.com>
2008-11-25 16:24:52 -0600

    Override composed_of setter to ignore blank values

--- lib/money/rails.rb
+++ lib/money/rails.rb
@@ -16,6 +16,11 @@ module ActiveRecord #:nodoc:
           composed_of name, :class_name => 'Money', :mapping => mapping, :allow_nil => true do |m|
             m.to_money
           end
+            
+          define_method "#{name}_with_blank=" do |value|
+            send("#{name}_without_blank=", value) unless value.blank?
+          end
+          alias_method_chain "#{name}=", :blank
         end
       end
     end
