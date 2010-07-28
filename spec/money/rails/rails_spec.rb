7a6b36449c98a9c150fd9c59bf16fcac1e8226a3
Todd Fisher <taf2@glazgo.(none)>
2010-07-28 14:57:27 -0400

    fix require path for 1.9.x

--- spec/money/rails/rails_spec.rb
+++ spec/money/rails/rails_spec.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/../../spec_helper'
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 require 'rubygems'
 require 'active_record'
 require 'money/rails'
