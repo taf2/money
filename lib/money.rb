5aa5a1066dc32e3ffeea6d0407a95104d3276e99
jphillips <jphillips@jphillips.local>
2010-09-02 12:07:37 -0400

    fixed comma bug with negative numbers

--- lib/money.rb
+++ lib/money.rb
@@ -126,6 +126,7 @@ class Money
     if rules.include?(:commas)
       first, last = amount.split('.')
       digits = first.split('')
+      minus = digits.shift if digits.first == '-'
       if digits.size > 3
         counter = 0
         digits_with_commas = []
@@ -138,6 +139,7 @@ class Money
           digits_with_commas << digit
         end
         amount = digits_with_commas.reverse.join + '.' + last
+        amount = "-" + amount if minus
       end
     end
 
